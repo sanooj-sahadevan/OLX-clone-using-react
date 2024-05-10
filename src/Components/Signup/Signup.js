f753b040188b88d95780c24b58422de689a93cd0
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-05-10 11:47:05 +0530

    add

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,19 +1,32 @@
-import React from 'react';
-
-import Logo from '../../olx-logo.png';
-import './Signup.css';
+import React, { useState, useContext } from 'react';
+import { FirebaseContext } from '../../store/FirebaseContext';
+import Logo from "../../olx-logo.png";
+import "./Signup.css";
 
 export default function Signup() {
+  const [userName, setUserName] = useState("");
+  const [Email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [Password, setPassword] = useState("");
+  const {firebase} = useContext(FirebaseContext)
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    console.log(firebase);
+  }
+
   return (
     <div>
       <div className="signupParentDiv">
         <img width="200px" height="200px" src={Logo}></img>
-        <form>
+        <form onSubmit={handleSubmit} >
           <label htmlFor="fname">Username</label>
           <br />
           <input
             className="input"
             type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
             id="fname"
             name="name"
             defaultValue="John"
@@ -25,6 +38,8 @@ export default function Signup() {
             className="input"
             type="email"
             id="fname"
+            value={Email}
+            onChange={(e) => setEmail(e.target.value)}
             name="email"
             defaultValue="John"
           />
@@ -35,6 +50,8 @@ export default function Signup() {
             className="input"
             type="number"
             id="lname"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
             name="phone"
             defaultValue="Doe"
           />
@@ -45,6 +62,8 @@ export default function Signup() {
             className="input"
             type="password"
             id="lname"
+            value={Password}
+            onChange={(e) => setPassword(e.target.value)}
             name="password"
             defaultValue="Doe"
           />
---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBvOqBOyWcEtz02GbJYdfRK0PqeiVLlYG8",
+    authDomain: "olx-clone-97fa3.firebaseapp.com",
+    projectId: "olx-clone-97fa3",
+    storageBucket: "olx-clone-97fa3.appspot.com",
+    messagingSenderId: "475710376075",
+    appId: "1:475710376075:web:08dd37237975991435ac8e",
+    measurementId: "G-B7XR15PEZE"
+  };
+
+export default firebase.initializeApp(firebaseConfig)
